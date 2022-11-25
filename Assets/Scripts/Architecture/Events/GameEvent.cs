1db33379b5def89dc5521d7dc86d7a96110292f2
ApotoxinElish <19079181d@connect.polyu.hk>
2022-11-25 17:40:38 +0800

    Update

--- Assets/Scripts/Architecture/Events/GameEvent.cs
+++ Assets/Scripts/Architecture/Events/GameEvent.cs
@@ -9,16 +9,24 @@ namespace Dune
         order = 0)]
     public class GameEvent : ScriptableObject
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private readonly List<GameEventListener> listeners = new List<GameEventListener>();
 
+        public void Raise()
+        {
+            for (var i = listeners.Count - 1; i >= 0; i--)
+                listeners[i].OnEventRaised();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void RegisterListener(GameEventListener listener)
         {
+            if (!listeners.Contains(listener))
+                listeners.Add(listener);
+        }
 
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (listeners.Contains(listener))
+                listeners.Remove(listener);
         }
     }
 }
