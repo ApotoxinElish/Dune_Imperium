21eba1bfb0c4ed8bb48026591b2a0942ba7c5b0a
ApotoxinElish <19079181d@connect.polyu.hk>
2022-11-27 21:16:32 +0800

    Update DeckDrawingSystem.cs

--- Assets/Scripts/Systems/DeckDrawingSystem.cs
+++ Assets/Scripts/Systems/DeckDrawingSystem.cs
@@ -14,14 +14,26 @@ namespace Dune
         private DeckWidget deckWidget;
         private DiscardPileWidget discardPileWidget;
 
-        private const int InitialDeckCapacity = 15;
-        private const int InitialDiscardPileCapacity = 15;
-        private const int InitialHandCapacity = 5;
+        private const int InitialDeckCapacity = 20;
+        private const int InitialDiscardPileCapacity = 20;
+        private const int InitialHandCapacity = 10;
 
         public void Initialize(DeckWidget deck, DiscardPileWidget discardPile)
         {
             deckWidget = deck;
             discardPileWidget = discardPile;
+        }
+
+        private void Start()
+        {
+            deck = new List<RuntimeCard>(InitialDeckCapacity);
+            discardPile = new List<RuntimeCard>(InitialDiscardPileCapacity);
+            hand = new List<RuntimeCard>(InitialHandCapacity);
+        }
+
+        public List<RuntimeCard> GetDeck()
+        {
+            return deck;
         }
 
         public int LoadDeck(List<CardTemplate> playerDeck)
